8223d9a8ad491d1c15c70cde3f0c9f56c5022372
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2020-04-08 21:02:27 +0300

    Update about answer
    Signed-off-by: Aliaksandr Shylau <alex.shilov.by@gmail.com>

--- app/interactions/about_answer.rb
+++ app/interactions/about_answer.rb
@@ -8,12 +8,13 @@ class AboutAnswer < ApplicationInteraction
   def text
     <<~DOC
       Data is provided by Johns Hopkins CSSE https://systems.jhu.edu/research/public-health/ncov/
-
       We update dataset every 15 minutes.
 
+      The program source code is open under the MIT license. 
+      You may contribute here https://github.com/Covid-Lab/covid_radar
       For any wishes or suggestions, please write to covidradar@yahoo.com
 
-      Please stay home, love loved ones and be healthy.
+      Stay home. Stay safe. Save lives.
     DOC
   end
 end
